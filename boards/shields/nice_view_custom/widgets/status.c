47a8d38b8ad3f4de097734c6c40f5c186284af39
David Sifry <david@sifry.com>
2025-03-03 00:06:05 -0800

    Remove art.h include from status widget

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -25,7 +25,6 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/endpoints.h>
 #include <zmk/keymap.h>
 #include <zmk/wpm.h>
-#include "art.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
