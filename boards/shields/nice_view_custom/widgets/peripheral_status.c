740fe6ff79018c6119f09e7876f309f165630d69
David Sifry <david@sifry.com>
2025-03-03 00:00:40 -0800

    Remove mountain artwork from status widget art selection

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -24,7 +24,6 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "peripheral_status.h"
 
 LV_IMG_DECLARE(balloon);
-LV_IMG_DECLARE(mountain);
 LV_IMG_DECLARE(jelllyfish);
 LV_IMG_DECLARE(seahorse);
 
@@ -117,8 +116,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     lv_obj_t *art = lv_img_create(widget->obj);
-    uint32_t random = sys_rand32_get() % 4;  // Get random number between 0-3
-    const lv_img_dsc_t* images[] = {&balloon, &mountain, &jelllyfish, &seahorse};
+    uint32_t random = sys_rand32_get() % 3;  // Get random number between 0-2
+    const lv_img_dsc_t* images[] = {&balloon, &jelllyfish, &seahorse};
     lv_img_set_src(art, images[random]);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
