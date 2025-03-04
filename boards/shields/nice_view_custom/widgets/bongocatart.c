b5a02dae92f7bc9ee5222e9c2885a0319bb2d3eb
David Sifry <david@sifry.com>
2025-03-04 08:56:28 -0800

    Add LVGL header include guards for Bongo Cat artwork widget

--- boards/shields/nice_view_custom/widgets/bongocatart.c
+++ boards/shields/nice_view_custom/widgets/bongocatart.c
@@ -1,3 +1,21 @@
+#ifdef __has_include
+    #if __has_include("lvgl.h")
+        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
+            #define LV_LVGL_H_INCLUDE_SIMPLE
+        #endif
+    #endif
+#endif
+
+#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
+
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
+
 #ifndef LV_ATTRIBUTE_IMG_BONGOCATREST0
 #define LV_ATTRIBUTE_IMG_BONGOCATREST0
 #endif
