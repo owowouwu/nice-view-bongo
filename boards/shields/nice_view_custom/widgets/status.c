9950c4d9f115db8c5d95d3fff1785fc3ceb81cfa
David Sifry <david@sifry.com>
2025-03-03 22:41:57 -0800

    Remove status state structure redefinition from status widget

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -62,15 +62,6 @@ LV_IMG_DECLARE(bongocatfast2);
 
 static uint8_t frame_counter = 0;
 
-struct status_state {
-    struct zmk_endpoint_instance selected_endpoint;
-    int active_profile_index;
-    bool active_profile_connected;
-    bool active_profile_bonded;
-    uint8_t wpm[10];
-    bool key_pressed;
-};
-
 static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
     lv_obj_t *canvas = lv_obj_get_child(widget, 0);
 
