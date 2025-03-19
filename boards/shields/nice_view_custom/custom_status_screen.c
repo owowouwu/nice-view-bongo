a458a18f677c1d5906bdd493e03032e06f7db605
David Sifry <david@sifry.com>
2025-03-19 00:25:44 -0700

    Refactor status screen by removing unnecessary modifiers widget code
    - Eliminate the inclusion of the modifiers widget as it is no longer needed, streamlining the code.
    - Update comments to reflect the removal of the modifiers widget and clarify the alignment of the status widget, enhancing overall code documentation.

--- boards/shields/nice_view_custom/custom_status_screen.c
+++ boards/shields/nice_view_custom/custom_status_screen.c
@@ -6,7 +6,8 @@
  */
 
 #include "widgets/status.h"
-#include "widgets/modifiers.h"
+// Remove this include since we no longer need it
+// #include "widgets/modifiers.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -15,9 +16,10 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 static struct zmk_widget_status status_widget;
 #endif
 
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-static struct zmk_widget_modifiers modifiers_widget;
-#endif
+// Remove this since modifiers are now part of status widget
+// #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+// static struct zmk_widget_modifiers modifiers_widget;
+// #endif
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
@@ -25,16 +27,8 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_status_init(&status_widget, screen);
-    // Position status widget at the top-left
-    // After 90-degree clockwise rotation, this will be at the top-right
-    lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
-#endif
-
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    zmk_widget_modifiers_init(&modifiers_widget, screen);
-    // Position modifiers widget to the right of the bluetooth circle
-    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), 
-                 LV_ALIGN_TOP_LEFT, 30, 13);  // Align horizontally with bluetooth circle
+    // Position the entire status widget
+    lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_LEFT_MID, 0, 0);
 #endif
 
     return screen;
