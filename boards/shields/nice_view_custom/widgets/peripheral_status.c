d631d3f52b08d2a890e9822b58b82396e568f905
Steven Nguyen <Steven Nguyen>
2025-03-24 22:10:18 +1100

    add custom art and remove the other cycling ones

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -23,9 +23,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "peripheral_status.h"
 
-LV_IMG_DECLARE(balloon);
-LV_IMG_DECLARE(jelllyfish);
-LV_IMG_DECLARE(seahorse);
+LV_IMG_DECLARE(rowlett);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -117,8 +115,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     uint32_t random = sys_rand32_get() % 3;  // Get random number between 0-2
-    const lv_img_dsc_t* images[] = {&balloon, &jelllyfish, &seahorse};
-    lv_img_set_src(art, images[random]);
+    const lv_img_dsc_t* image_to_draw = &rowlett 
+    lv_img_set_src(art, image_to_draw);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
